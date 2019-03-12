2a4dede39809e21cdf208364eb1abe1d97097cb5
Jacek Lajdecki <lok7ok@gmail.com>
2019-03-12 23:52:41 +0100

    Bug Fixes, better code

--- OOPGame/BattleGame.java
+++ OOPGame/BattleGame.java
@@ -1,234 +1,184 @@
 package OOPGame;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Random;
 import java.util.Scanner;
 import OOPGame.menuText;
 
-public class BattleGame
-{
-  public static void main(String args[]) throws InterruptedException, IOException
-  {
-    menuText.testPrinting("HomeScreen.txt");
-  }
+public class BattleGame {
+	public static void main(String args[]) throws InterruptedException, IOException {
+		menuText.printingText("HomeScreen.txt");
+	}
 
-  static void startGame() throws InterruptedException
-  {
-    Scanner user_input = new Scanner(System.in);
+	static void startGame() throws InterruptedException, FileNotFoundException, IOException {
+		Scanner user_input = new Scanner(System.in);
 
-    System.out.print("Name the first warrior:");
-    String input1 = user_input.next();
-    Warrior warrior1 = new Warrior(input1, 100, 100, 100);
-    Warrior.classChanger(warrior1, chosingClass(user_input));
-    
-    System.out.print("Name the second warrior:");
-    String input2 = user_input.next();
-    Warrior warrior2 = new Warrior(input2, 100, 100, 100);
-    Warrior.classChanger(warrior2, chosingClass(user_input));
-    
-    checkWarriorName(warrior1, warrior2);
-    Battle.startFight(warrior1, warrior2);
-  }
+		System.out.print("Name the first warrior:");
+		String input1 = user_input.next();
+		Warrior warrior1 = new Warrior(input1, 100, 100, 100);
+		Warrior.classChanger(warrior1, chosingClass(user_input));
 
-  private static void checkWarriorName(Warrior warrior1, Warrior warrior2) throws InterruptedException
-  {
-    if (warrior1.name.equals(warrior2.name))
-    {
-      warrior1.name += "1";
-      warrior2.name += "2";
-      System.out.println("Unfortunately warriors names are the same so we changed it to "+warrior1.name+" and "+warrior2.name);
-      Thread.sleep(1000);
-    }
-  }
+		System.out.print("Name the second warrior:");
+		String input2 = user_input.next();
+		Warrior warrior2 = new Warrior(input2, 100, 100, 100);
+		Warrior.classChanger(warrior2, chosingClass(user_input));
 
-  private static int chosingClass(Scanner user_input)
-  {
-    menuText.testPrinting("ClassChooser.txt");
-    while (!user_input.hasNextInt())
-    {
-      System.out.println("Input is not a number!");
-      user_input.nextLine();
-    }
-    return user_input.nextInt();
-  }
+		checkWarriorName(warrior1, warrior2);
+		Battle.startFight(warrior1, warrior2);
+	}
+
+	private static void checkWarriorName(Warrior warrior1, Warrior warrior2) throws InterruptedException {
+		if (warrior1.name.equals(warrior2.name)) {
+			warrior1.name += "1";
+			warrior2.name += "2";
+			System.out.println("Unfortunately warriors names are the same so we changed it to " + warrior1.name
+					+ " and " + warrior2.name);
+			Thread.sleep(1000);
+		}
+	}
+
+	private static int chosingClass(Scanner user_input) {
+		menuText.printingText("ClassChooser.txt");
+		while (!user_input.hasNextInt()) {
+			System.out.println("Input is not a number!");
+			user_input.nextLine();
+		}
+		return user_input.nextInt();
+	}
 }
 
-class Warrior
-{
+class Warrior {
 
-  public String name;
-  public int health;
-  public int attkMax;
-  public int blckMax;
-  public int healthPotions = 3;
+	public String name;
+	public int health;
+	public int attkMax;
+	public int blckMax;
+	public int healthPotions = 3;
 
-  public Warrior(String Name, int Health, int AttkMax, int BlckMax)
-  {
-    name = Name;
-    health = Health;
-    attkMax = AttkMax;
-    blckMax = BlckMax;
-  }
+	public Warrior(String Name, int Health, int AttkMax, int BlckMax) {
+		name = Name;
+		health = Health;
+		attkMax = AttkMax;
+		blckMax = BlckMax;
+	}
 
-  public static void classChanger(Warrior warrior, int chosed)
-  {
-    if (chosed == 1)
-    {
-      warrior.health = 1000;
-      warrior.attkMax = 140;
-      warrior.blckMax = 30;
-    }
-    else if (chosed == 2)
-    {
-      warrior.health = 1200;
-      warrior.attkMax = 100;
-      warrior.blckMax = 60;
-    }
-    else if (chosed == 3)
-    {
-      warrior.health = 700;
-      warrior.attkMax = 200;
-      warrior.blckMax = 20;
-    }
-    else {
-      System.out.println("Wrong class!");
-    }
-  }
+	public static void classChanger(Warrior warrior, int chosed) {
+		if (chosed == 1) {
+			warrior.health = 1000;
+			warrior.attkMax = 140;
+			warrior.blckMax = 30;
+		} else if (chosed == 2) {
+			warrior.health = 1200;
+			warrior.attkMax = 100;
+			warrior.blckMax = 60;
+		} else if (chosed == 3) {
+			warrior.health = 700;
+			warrior.attkMax = 200;
+			warrior.blckMax = 20;
+		} else {
+			System.out.println("Wrong class!");
+		}
+	}
 
-  public static int Attack(Warrior warrior)
-  {
-    return new Random().nextInt(warrior.attkMax + 1 - 0) + 0;
-  }
+	public static int Attack(Warrior warrior) {
+		return new Random().nextInt(warrior.attkMax + 1 - 0) + 0;
+	}
 
-  public static int Block(Warrior warrior)
-  {
-    return new Random().nextInt(warrior.blckMax + 1 - 0) + 0;
-  }
+	public static int Block(Warrior warrior) {
+		return new Random().nextInt(warrior.blckMax + 1 - 0) + 0;
+	}
 
-  public static int HealValue(Warrior warrior)
-  {
-    warrior.healthPotions -= 1;
-    return new Random().nextInt(100 + 1 - 20) + 20;
-  }
+	public static int HealValue(Warrior warrior) {
+		warrior.healthPotions -= 1;
+		return new Random().nextInt(100 + 1 - 20) + 20;
+	}
 }
 
-class Battle
-{
-  public static void startFight(Warrior warriorA, Warrior warriorB) throws InterruptedException
-  {
-    Scanner actionChoser = new Scanner(System.in);
-    do
-    {
-      actionMenu(warriorA, warriorB, actionChoser);
-      actionMenu(warriorB, warriorA, actionChoser);
-    } while (warriorA.health > 0 && warriorB.health > 0);
-    checkWhoWin(warriorA, warriorB);
-  }
+class Battle {
+	public static void startFight(Warrior warriorA, Warrior warriorB)
+			throws InterruptedException, FileNotFoundException, IOException {
+		Scanner actionChoser = new Scanner(System.in);
+		do {
+			actionMenu(warriorA, warriorB, actionChoser);
+			actionMenu(warriorB, warriorA, actionChoser);
+		} while (warriorA.health > 0 && warriorB.health > 0);
+		checkWhoWin(warriorA, warriorB);
+	}
 
-  private static void actionMenu(Warrior warriorA, Warrior warriorB, Scanner actionChoser)
-          throws InterruptedException
-  {
-    System.out.println("\n================================\n"
-            + warriorA.name + " choose what you want to do:"
-            + "\n[1] Attack\n[2] Block\n[3] Heal Potion"
-            + "\n================================");
-  //  Path path = Paths.get("TextMenus/BattleActionMenu.txt");
-    
-    
-    checkIfCanDoAction(warriorA, actionChoser);
-    int action1 = actionChoser.nextInt();
-    actionGoing(warriorA, warriorB, action1);
-    Thread.sleep(1500);
-  }
+	private static void actionMenu(Warrior warriorA, Warrior warriorB, Scanner actionChoser)
+			throws InterruptedException, FileNotFoundException, IOException {
+		String menu = menuText.filePrinter("BattleActionMenu.txt");
+		System.out.println(menu.replace("warriorName", warriorA.name));
 
-  private static void checkIfCanDoAction(Warrior warriorA, Scanner actionChoser)
-  {
-    while (!actionChoser.hasNextInt())
-    {
-      System.out.println("Input is not a number!");
-      actionChoser.nextLine();
-    }
-    while (!(warriorA.healthPotions > 0))
-    {
-      System.out.println("You dont have any Health Potions left.");
-      actionChoser.nextInt();
-    }
-  }
+		checkIfCanDoAction(warriorA, actionChoser);
+		int action1 = actionChoser.nextInt();
+		actionGoing(warriorA, warriorB, action1);
+		Thread.sleep(500);
+	}
 
-  private static void actionGoing(Warrior warrior1, Warrior warrior2, int action) throws InterruptedException
-  {
-    if (action == 1)
-    {
-      getAttackResult(warrior1, warrior2, damageRandomizer(warrior1, warrior2, action));
-      checkWhoWin(warrior1, warrior2);
-      getAttackResult(warrior2, warrior1, damageRandomizer(warrior2, warrior1, 0));
-    }
-    else if (action == 2)
-    {
-      getAttackResult(warrior1, warrior2, damageRandomizer(warrior1, warrior2, 0));
-      checkWhoWin(warrior1, warrior2);
-      getAttackResult(warrior2, warrior1, damageRandomizer(warrior2, warrior1, action));
-    }
-    else if (action == 3)
-    {
-      int heal = Warrior.HealValue(warrior1);
-      warrior1.health += heal;
-      System.out.println("Warrior " + warrior1.name + " healed for " + heal + ". Current Health is "
-              + warrior1.health + "\nYou have " + warrior1.healthPotions + " Health Potions left");
-    }
-  }
+	private static void checkIfCanDoAction(Warrior warriorA, Scanner actionChoser) {
+		while (!actionChoser.hasNextInt()) {
+			System.out.println("Input is not a number!");
+			actionChoser.nextLine();
+		}
+		while (!(warriorA.healthPotions > 0)) {
+			System.out.println("You dont have any Health Potions left.");
+			actionChoser.nextInt();
+		}
+	}
 
-  private static void getAttackResult(Warrior warriorA, Warrior warriorB, int damage)
-          throws InterruptedException
-  {
-    if (damage <= 0)
-    {
-      System.out.println("\nWarrior " + warriorB.name + " blocked the attack!");
-      Thread.sleep(1000);
-    }
-    else
-    {
-      System.out.println("\nWarrior " + warriorA.name + " did " + damage + " to " + warriorB.name);
-      warriorB.health -= damage;
-      Thread.sleep(500);
-      System.out.println("Warrior " + warriorB.name + " has " + warriorB.health + " Health.");
-      Thread.sleep(1000);
-    }
-  }
+	private static void actionGoing(Warrior warrior1, Warrior warrior2, int action) throws InterruptedException {
+		if (action == 1) {
+			getAttackResult(warrior1, warrior2, damageRandomizer(warrior1, warrior2, action));
+			checkWhoWin(warrior1, warrior2);
+			getAttackResult(warrior2, warrior1, damageRandomizer(warrior2, warrior1, 0));
+		} else if (action == 2) {
+			getAttackResult(warrior1, warrior2, damageRandomizer(warrior1, warrior2, 0));
+			checkWhoWin(warrior1, warrior2);
+			getAttackResult(warrior2, warrior1, damageRandomizer(warrior2, warrior1, action));
+		} else if (action == 3) {
+			int heal = Warrior.HealValue(warrior1);
+			warrior1.health += heal;
+			System.out.println("Warrior " + warrior1.name + " healed for " + heal + ". Current Health is "
+					+ warrior1.health + "\nYou have " + warrior1.healthPotions + " Health Potions left");
+		}
+	}
 
-  public static int damageRandomizer(Warrior warrior1, Warrior warrior2, int buff)
-  {
-    int war1AtkAmt = Warrior.Attack(warrior1);
-    int war2BlkAmt = Warrior.Block(warrior2);
-    if (buff == 1)
-    {
-      war1AtkAmt += 30;
-    }else if (buff == 2)
-    {
-      war2BlkAmt += 30;
-    }
-    int damage = war1AtkAmt - war2BlkAmt;
-    return damage;
-  }
+	private static void getAttackResult(Warrior warriorA, Warrior warriorB, int damage) throws InterruptedException {
+		if (damage <= 0) {
+			System.out.println("\nWarrior " + warriorB.name + " blocked the attack!");
+			Thread.sleep(1000);
+		} else {
+			System.out.println("\nWarrior " + warriorA.name + " did " + damage + " to " + warriorB.name);
+			warriorB.health -= damage;
+			Thread.sleep(500);
+			System.out.println("Warrior " + warriorB.name + " has " + warriorB.health + " Health.");
+			Thread.sleep(1000);
+		}
+	}
 
-  private static void checkWhoWin(Warrior warrior1, Warrior warrior2)
-  {
-    if (warrior1.health <= 0)
-    {
-      System.out.println("\nWarrior " + warrior1.name + " has died and " + warrior2.name + " is Victorious!");
-      return;
-    }
-    else if (warrior2.health <= 0)
-    {
-      System.out.println("\nWarrior " + warrior2.name + " has died and " + warrior1.name + " is Victorious!");
-      return;
-    }
-    else
-    {
-      return;
-    }
-  }
+	public static int damageRandomizer(Warrior warrior1, Warrior warrior2, int buff) {
+		int war1AtkAmt = Warrior.Attack(warrior1);
+		int war2BlkAmt = Warrior.Block(warrior2);
+		if (buff == 1) {
+			war1AtkAmt += 30;
+		} else if (buff == 2) {
+			war2BlkAmt += 30;
+		}
+		int damage = war1AtkAmt - war2BlkAmt;
+		return damage;
+	}
+
+	private static void checkWhoWin(Warrior warrior1, Warrior warrior2) {
+		if (warrior1.health <= 0) {
+			System.out.println("\nWarrior " + warrior1.name + " has died and " + warrior2.name + " is Victorious!");
+			return;
+		} else if (warrior2.health <= 0) {
+			System.out.println("\nWarrior " + warrior2.name + " has died and " + warrior1.name + " is Victorious!");
+			return;
+		} else {
+			return;
+		}
+	}
 }
--- OOPGame/menuText.java
+++ OOPGame/menuText.java
@@ -1,93 +1,80 @@
 package OOPGame;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
-import sun.net.NetworkServer;
+public class menuText {
+	public static String filePrinter(String fileName) throws FileNotFoundException, IOException 
+	{
+		Path path = Paths.get("TextMenus/" + fileName);
+		return new String(Files.readAllBytes(path));
+	}
 
-public class menuText
-{
-  public static String filePrinter(String fileName) throws FileNotFoundException, IOException
-  {
-    Path path = Paths.get("TextMenus/"+fileName);
-    return new String (Files.readAllBytes(path));
-    
-    /*
-    try (BufferedReader br = new BufferedReader(new FileReader("TextMenus/"+fileName)))
-    {
-      String line = null;
-      while ((line = br.readLine()) != null)
-      {
-        System.out.println(line);
-      }
-      return;
-    }*/
-  }
+	private static void spacing() 
+	{
+		for (int i = 0; i < 20; i++) 
+		{
+			System.out.println("");
+		}
+	}
 
-  private static void spacing()
-  {
-    for (int i = 0; i < 20; i++)
-    {
-      System.out.println("");
-    }
-  }
+	@SuppressWarnings("resource")
+	private static void printSubMenu() throws InterruptedException, FileNotFoundException, IOException 
+	{
+		System.out.println(filePrinter("MainMenu.txt"));
+		Scanner input = new Scanner(System.in);
+		int scan = input.nextInt();
+		spacing();
+		subMenuChose(scan);
+	}
 
-  @SuppressWarnings("resource")
-  public static void actionMenu() throws FileNotFoundException, IOException
-  {
-    System.out.println(filePrinter("BattleActionMenu.txt"));
-    new Scanner(System.in).nextLine();
-  }
+	private static void subMenuChose(int scan) throws InterruptedException, FileNotFoundException, IOException 
+	{
+		if (scan == 1) 
+		{
+			BattleGame.startGame();
+		} 
+		else if (scan == 2) 
+		{
+			printingText("Instructions.txt");
+		} 
+		else if (scan == 3) 
+		{
+			printingText("Credits.txt");
+		} 
+		else if (scan == 4) 
+		{
+			printingText("ClassStats.txt");
+		}
+	}
+	
+	private static String[] menus = {"BattleActionMenu.txt", "ClassChooser.txt"};
 
-  @SuppressWarnings("resource")
-  private static void printSubMenu() throws InterruptedException, FileNotFoundException, IOException
-  {
-    System.out.println(filePrinter("MainMenu.txt"));
-    Scanner input = new Scanner(System.in);
-    int scan = input.nextInt();
-    spacing();
-    subMenuChose(scan);
-  }
-
-  private static void subMenuChose(int scan) throws InterruptedException
-  {
-    if (scan == 1)
-    {
-      BattleGame.startGame();
-    }
-    else if (scan == 2)
-    {
-      testPrinting("Instructions.txt");
-    }
-    else if (scan == 3)
-    {
-      testPrinting("Credits.txt");
-    }
-    else if (scan == 4)
-    {
-      testPrinting("ClassStats.txt");
-    }
-  }
-  
-  @SuppressWarnings("resource")
-  public static void testPrinting(String filename)
-  {
-    try
-    {
-      System.out.println(filePrinter(filename));
-      new Scanner(System.in).nextLine();
-      spacing();
-      printSubMenu();
-    }
-    catch (Exception ex)
-    {
-      ex.printStackTrace();
-    }
-  }
+	public static void printingText(String filename) 
+	{
+		try 
+		{
+			System.out.println(filePrinter(filename));
+			while (!checkIfStringContainsList(filename)) 
+			{
+				new Scanner(System.in).nextLine();
+				spacing();
+				printSubMenu();
+			}
+		} 
+		catch (Exception ex) 
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	public static boolean checkIfStringContainsList(String input)
+	{
+		return Arrays.stream(menus).parallel().anyMatch(input::contains);
+	}
 }
