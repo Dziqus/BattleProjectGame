5f89c29c4cdd698051bf6961f6e809c70acc31f4
Jacek Lajdecki <jacek.lajdecki@axonivy.com>
2019-04-17 16:10:02 +0200

    Add first JUnit test
    need to be finished

---
+++ oopGameTest/BattleTest.java
@@ -0,0 +1,22 @@
+package oopGameTest;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import oopGame.Warrior;
+
+public class BattleTest
+{
+
+  @Test
+  public static void test() 
+  {
+    Warrior wizzard = new Warrior("wizzard", 1, 200, 20, 0, "wizzard");
+    ArrayList<Integer> damages = new ArrayList<Integer>();
+    for (int i = 0; i < 20; i++)
+    {
+      damages.add(Warrior.attack(wizzard));
+    }
+  }
+}
